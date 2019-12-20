1f9da2e9cb1af32a4768958451efd1cf1de85ee8
rbazua <Ricardo.Bazua@Tideworks.com>
2019-12-20 10:47:06 -0600

    WishiPets Android app with navigation and layouts

---
+++ bin/apps/android/WishiPets/app/src/main/java/com/rbazua/wishipets/view/FeederFragment.kt
@@ -0,0 +1,23 @@
+package com.rbazua.wishipets.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import com.rbazua.wishipets.R
+import kotlinx.android.synthetic.main.fragment_feeder.*
+
+class FeederFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feeder, container, false)
+    }
+}
